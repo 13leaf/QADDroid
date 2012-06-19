a11673c87d5b3ef7665ac74a37bbc04753ca2557
wangfeng <wangfeng@8e18ede0-3dff-4a33-a72d-e2b074c0484d>
2012-06-19 09:53:20 +0000

    为Http层增加透明gzip的功能
    git-svn-id: http://211.151.61.118/svn/ifengclient_android/trunk/QADFramework@1387 8e18ede0-3dff-4a33-a72d-e2b074c0484d

--- src/com/qad/net/HttpManagerImpl.java
+++ src/com/qad/net/HttpManagerImpl.java
@@ -10,18 +10,29 @@ import java.net.ProtocolException;
 import java.net.Proxy;
 import java.net.URL;
 import java.net.Proxy.Type;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.entity.HttpEntityWrapper;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
 
 import com.qad.lang.Streams;
 
@@ -81,6 +92,31 @@ class HttpManagerImpl implements IHttpManager {
 //				&& android.net.Proxy.getDefaultPort() != -1;
 		return ApnManager.useProxy;
 	}
+	
+	private final static class GzipDecompressingEntity extends HttpEntityWrapper {
+
+        public GzipDecompressingEntity(final HttpEntity entity) {
+            super(entity);
+        }
+
+        @Override
+        public InputStream getContent()
+            throws IOException, IllegalStateException {
+
+            // the wrapped entity's getContent() decides about repeatability
+            InputStream wrappedin = wrappedEntity.getContent();
+
+            return new GZIPInputStream(wrappedin);
+        }
+
+        @Override
+        public long getContentLength() {
+            // length of ungzipped content is not known
+            return -1;
+        }
+
+    }
+
 
 	/* (non-Javadoc)
 	 * @see com.qad.net.IHttpManager#getHttpClient()
@@ -94,6 +130,38 @@ class HttpManagerImpl implements IHttpManager {
 			httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
 					proxy);
 		}
+		//ensure gzipablity
+		httpclient.addRequestInterceptor(new HttpRequestInterceptor() {
+
+            public void process(
+                    final HttpRequest request,
+                    final HttpContext context) throws HttpException, IOException {
+                if (!request.containsHeader("Accept-Encoding")) {
+                    request.addHeader("Accept-Encoding", "gzip");
+                }
+            }
+
+        });
+        httpclient.addResponseInterceptor(new HttpResponseInterceptor() {
+
+            public void process(
+                    final HttpResponse response,
+                    final HttpContext context) throws HttpException, IOException {
+                HttpEntity entity = response.getEntity();
+                Header ceheader = entity.getContentEncoding();
+                if (ceheader != null) {
+                    HeaderElement[] codecs = ceheader.getElements();
+                    for (int i = 0; i < codecs.length; i++) {
+                        if (codecs[i].getName().equalsIgnoreCase("gzip")) {
+                            response.setEntity(
+                                    new GzipDecompressingEntity(response.getEntity()));
+                            return;
+                        }
+                    }
+                }
+            }
+
+        });
 		return httpclient;
 	}
 
@@ -109,6 +177,8 @@ class HttpManagerImpl implements IHttpManager {
 		}
 		return connection.getInputStream();
 	}
+	
+	final static Pattern GZIP_PATTERN=Pattern.compile("gzip", Pattern.CASE_INSENSITIVE);
 
 	private HttpURLConnection getHttpUrlConnection(String url)
 			throws MalformedURLException, IOException, ProtocolException {
@@ -128,6 +198,7 @@ class HttpManagerImpl implements IHttpManager {
 		connection.setDoInput(true);
 		connection.setUseCaches(false);
 		connection.setRequestMethod("GET");
+		connection.addRequestProperty("Accept-Encoding", "gzip");
 		connection.connect();
 		return connection;
 	}
@@ -137,8 +208,15 @@ class HttpManagerImpl implements IHttpManager {
 	 */
 	@Override
 	public String getHttpText(String url) throws IOException {
+		HttpURLConnection connection=getHttpUrlConnection(url);
+		InputStream is=connection.getInputStream();
+		String encoding=connection.getHeaderField("Content-Encoding");
+		if(GZIP_PATTERN.matcher(encoding).find())
+		{
+			is=new GZIPInputStream(is);
+		}
 		return Streams.readAndClose(new InputStreamReader(Streams
-				.buff(getInputStream(url))));
+				.buff(is)));
 	}
 
 	@Override
